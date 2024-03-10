5edf24bc420c6479040a3daeca63c745db1b4694
mdervaux <maxime.dervaux@ittalent.fr>
2024-03-10 20:46:06 +0100

    Ajout RessourceBar(fav,archive)

--- screens/ressource.screen.js
+++ screens/ressource.screen.js
@@ -1,61 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView, View, Text, StyleSheet, Image, Dimensions, SafeAreaView } from "react-native";
 import { colors } from "../config/color";
-import Button from "../components/Button";
 import { fonts } from "../config/font";
+import RessourceBar from "../components/RessourceBar";
 
 const { width } = Dimensions.get("window"); 
 
-const apiUrl = process.env.EXPO_PUBLIC_API_URL+'/ressources/1';
+const idRessource = 1; // À changer une fois la page multi-ressource finie
+const apiUrl = process.env.EXPO_PUBLIC_API_URL + '/ressources/' + idRessource;
 
 const RessourceScreen = () => {
   const [resourceData, setResourceData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchResourceData = async () => {
       try {
         const response = await fetch(apiUrl);
-  
-        if (!response.ok) {
-          throw new Error('Failed to fetch resource data');
-        }
-  
         const data = await response.json();
         setResourceData(data.item); 
       } catch (error) {
         console.error('Error fetching resource data:', error);
+        setError(error.message);
       }
     };
   
     fetchResourceData();
   }, []);
 
-  const imageHeight = (180 / 350) * width;
+  // Gestion en cas d'erreur
+  if (error) {
+    return (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+    );
+  }
+
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View style={styles.imageContainer}>
+        <RessourceBar idRessource={idRessource} />
+        <View style={[styles.imageContainer, styles.shadowProp]}>
           <Image
             source={{ uri: resourceData?.url_res }}
-            style={{ width: width, height: imageHeight }}
+            style={{ width: "100%", aspectRatio: 16 / 9  }}
             resizeMode="cover"
+
           />
         </View>
-        <View style={styles.caracteristicProp}>
-            <Text style={[styles.category,fonts.caption]}>Catégorie:</Text>
-            <Text style={[styles.typeOfRelation,fonts.caption]}>Types de relations:</Text>
-            <Text style={[styles.typeOfResource,fonts.caption]}>Type de ressource:</Text>
+        <View style={styles.characteristicProp}>
+          <Text style={[styles.category, fonts.caption]}>Catégorie:</Text>
+          <Text style={[styles.typeOfRelation, fonts.caption]}>Types de relations:</Text>
+          <Text style={[styles.typeOfResource, fonts.caption]}>Type de ressource:</Text>
         </View>
-        <View style={[styles.content,styles.shadowProp]}>
+        <View style={styles.content}>
           <Text style={[styles.title, fonts.h1]}>{resourceData?.titre_res}</Text>
           <Text style={[styles.resourceContent, fonts.body]}>{resourceData?.contenu_res}</Text>
         </View>
-  
       </ScrollView>
-      <View style={[styles.buttonContent]}>
-        <Button theme="primary" label="Retour aux articles"></Button>
-      </View>
     </SafeAreaView>
   );
 };
@@ -65,32 +69,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.backgroundPrimary,
   },
-  caracteristicProp:{
-    height:100,
+  characteristicProp: {
+    height: 100,
     borderBottomWidth: 2, 
     borderBottomColor: colors.secondary, 
     marginLeft: 2,
     marginRight: 50 ,
     justifyContent: 'space-between',
-    paddingTop:10,
+    paddingTop: 10,
     paddingBottom: 10,
-    paddingLeft : 8
+    paddingLeft: 8,
+    width: '100%'
   },
   title: {
     fontSize: 20,
     marginLeft: 10,
-    fontWeight: "bold",                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    fontWeight: "bold",
     marginBottom: 10,
     color: colors.textPrimary,
   },
-
   imageContainer: {
     position: 'relative',
   },
   content: {
     padding: 8,
   },
-
   resourceContent: {
     fontSize: 17,
     paddingTop: 5,
@@ -98,22 +101,24 @@ const styles = StyleSheet.create({
     color: colors.textPrimary,
     textAlign: 'justify',
   },
-
-  buttonContent: {
-    paddingTop: 10,
-    paddingBottom: 10,
-    alignItems: 'center',
-  },
   shadowProp: {
-    width: "auto",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 4,
     },
-    shadowOpacity: 0.57,
-    shadowRadius: 15.19,
-    elevation: 23,
+    shadowOpacity: 0.15,
+    shadowRadius: 5,
+    elevation: 8,
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    fontSize: 16,
+    color: 'red',
   },
 });
 
